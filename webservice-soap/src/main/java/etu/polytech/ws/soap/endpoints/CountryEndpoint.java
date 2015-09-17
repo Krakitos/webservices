3747da8393ff5fabec93327dd14cea8b04409d0c
Morgan Funtowicz <funtowiczmo@gmail.com>
2015-09-17 20:05:34 +0200

    Better logging for CountryEndpoint

--- webservice-soap/src/main/java/etu/polytech/ws/soap/endpoints/CountryEndpoint.java
+++ webservice-soap/src/main/java/etu/polytech/ws/soap/endpoints/CountryEndpoint.java
@@ -1,5 +1,6 @@
 package etu.polytech.ws.soap.endpoints;
 
+import etu.polytech.ws.soap.domain.CountryEntity;
 import etu.polytech.ws.soap.lang.Country;
 import etu.polytech.ws.soap.lang.GetCountryRequest;
 import etu.polytech.ws.soap.lang.GetCountryResponse;
@@ -9,6 +10,8 @@ import org.jdom2.Namespace;
 import org.jdom2.filter.Filters;
 import org.jdom2.xpath.XPathExpression;
 import org.jdom2.xpath.XPathFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
@@ -22,6 +25,9 @@ import java.util.Optional;
  */
 @Endpoint
 public class CountryEndpoint {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CountryEndpoint.class);
+
     public static final String NAMESPACE_URI = "http://etu/polytech/ws/soap/lang";
 
     private final XPathExpression<String> nameExpression;
@@ -39,12 +45,26 @@ public class CountryEndpoint {
         nameExpression = xPathFactory.compile("//pays:name", Filters.fstring(), null, namespace);
     }
 
-    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "CountryRequest")
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
     @ResponsePayload
     public GetCountryResponse handlePaysRequest(@RequestPayload GetCountryRequest countryRequest)throws Exception {
+
+        if(LOGGER.isDebugEnabled())
+            LOGGER.debug("Received SOAP GetCountryRequest for country {}", countryRequest.getName());
+
         GetCountryResponse response = new GetCountryResponse();
-        Optional<Country> country = countryRepository.findCountry(countryRequest.getName());
-        country.ifPresent(response::setCountry);
+        Optional<CountryEntity> entity = Optional.ofNullable(countryRepository.findOne(countryRequest.getName().toUpperCase()));
+        entity.ifPresent(c -> {
+
+            if(LOGGER.isDebugEnabled())
+                LOGGER.debug("Found country in the database : {}", c);
+
+            Country country = new Country();
+            country.setName(c.name());
+            country.setCapital(c.capital());
+            country.setPopulation(c.population());
+            response.setCountry(country);
+        });
 
         return response;
     }
