2f7e96f3f2204a0045e7a6516fef3c71e6a7b69c
Morgan Funtowicz <funtowiczmo@gmail.com>
2015-09-17 20:05:21 +0200

    Use Spring Data CrudRepository

--- webservice-soap/src/main/java/etu/polytech/ws/soap/services/CountryRepository.java
+++ webservice-soap/src/main/java/etu/polytech/ws/soap/services/CountryRepository.java
@@ -1,36 +1,12 @@
 package etu.polytech.ws.soap.services;
 
-import etu.polytech.ws.soap.lang.Country;
-import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
-import java.util.*;
+import etu.polytech.ws.soap.domain.CountryEntity;
+import org.springframework.data.repository.CrudRepository;
 
 /**
  * Created by momo- on 16/09/2015.
  */
-@Component
-public class CountryRepository {
 
-    private Set<Country> countries = new HashSet<>();
+public interface CountryRepository extends CrudRepository<CountryEntity, String> {
 
-    @PostConstruct
-    public void initRepository(){
-
-    }
-
-    /**
-     * Return all countries
-     */
-    public Collection<Country> getAllCountry(){
-        return countries;
-    }
-
-    /**
-     * Get a country by name
-     * @param name Name of the country
-     */
-    public Optional<Country> findCountry(String name){
-        return Optional.empty();
-    }
 }
