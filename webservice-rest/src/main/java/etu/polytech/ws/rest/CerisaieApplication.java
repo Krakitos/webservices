a8460e0c06e1d7f40bd1ff4a2a0942ddeda1789f
Morgan Funtowicz <funtowiczmo@gmail.com>
2015-10-06 12:45:03 +0200

    Added configuration of the application

--- webservice-rest/src/main/java/etu/polytech/ws/rest/CerisaieApplication.java
+++ webservice-rest/src/main/java/etu/polytech/ws/rest/CerisaieApplication.java
@@ -2,14 +2,22 @@ package etu.polytech.ws.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Created by mfuntowicz on 20/09/15.
  */
 @SpringBootApplication
-public class CerisaieApplication {
+public class CerisaieApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CerisaieApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CerisaieApplication.class);
     }
 }
