57e6b84e340c6cbca8cbedc017df4cbe877ee704
Morgan Funtowicz <funtowiczmo@gmail.com>
2016-01-13 19:37:10 +0100

    Added View Resolver bean

---
+++ webservice-soap/src/main/java/etu/polytech/ws/soap/config/WebMvcConfiguration.java
@@ -0,0 +1,28 @@
+package etu.polytech.ws.soap.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+/**
+ * Created by momo- on 13/01/2016.
+ */
+@EnableWebMvc
+@Configuration
+public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Bean
+    public InternalResourceViewResolver getViewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+}
