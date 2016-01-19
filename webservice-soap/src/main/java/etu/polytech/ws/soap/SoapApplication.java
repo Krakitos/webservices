2e59d07e841d7843884001fd2248ac44f8063817
Morgan Funtowicz <funtowiczmo@gmail.com>
2016-01-19 17:19:57 +0100

    Annotation driven configuration

--- webservice-soap/src/main/java/etu/polytech/ws/soap/SoapApplication.java
+++ webservice-soap/src/main/java/etu/polytech/ws/soap/SoapApplication.java
@@ -1,14 +1,18 @@
 package etu.polytech.ws.soap;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by momo- on 16/09/2015.
  */
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 public class SoapApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
