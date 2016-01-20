46fd499d1a86171eaf6005555365c32ca771a9c1
Mickael <shah.mickael@gmail.com>
2016-01-20 08:38:40 +0100

    Update for docker

---
+++ webservice-rest/src/main/webapp/js/app.js
@@ -0,0 +1,4 @@
+/**
+ * Created by mfuntowicz on 23/12/15.
+ */
+angular.module('cerisaieapp', ['ngRoute', 'clientsControllers', 'clientsServices', 'sejoursControllers', 'sejoursServices']);
---
+++ webservice-rest/src/main/webapp/js/services.js
@@ -0,0 +1,29 @@
+/**
+ * Created by mfuntowicz on 23/12/15.
+ */
+
+var clientsServices = angular.module('clientsServices', ['ngResource']);
+
+clientsServices.factory('Client', ['$resource',
+    function($resource){
+        return $resource('api/clients/:id', { id: '@id'}, {
+            query:  {method:'GET', params:{}, isArray:true},
+            save:   {method:'PUT', params:{}, isArray:false},
+            delete: {method:'DELETE', params:{id: '@id'}, isArray:false},
+            update: {method: 'POST', params:{id: '@id'}, isArray:false},
+            get:    {method: 'GET', params:{id: '@id'}, isArray:false}
+        });
+    }]
+);
+
+var sejoursServices = angular.module('sejoursServices', ['ngResource']);
+
+sejoursServices.factory('Sejour', ['$resource',
+    function($resource){
+        return $resource('api/booking/:id', { id: '@id'}, {
+            query: {method:'GET', params:{}, isArray:true},
+            save: {method:'PUT', params:{}, isArray:false},
+            delete: {method:'DELETE', isArray:false}
+        });
+    }]
+);
